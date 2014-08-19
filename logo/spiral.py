057e73417b84ead2da78dac3e49f035d1c380317
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 11:43:14 +0100

    Running logo/spiral.py

--- logo/spiral.py
+++ logo/spiral.py
@@ -1,12 +1,12 @@
 from tealight.logo import move, turn
 
-def spiral(size):
+def spiral(size):  #function with var size
   
-  if size > 300:
+  if size > 300:   #end function once var size > 
     return
   
-  move(size)
-  turn(90)
-  spiral(size + 5)
+  move(size)       #move  size pixels
+  turn(90)         #rotate
+  spiral(size + 5) #increase size by 5
   
-spiral(0)
+spiral(0)          #call spiral with size = 0
