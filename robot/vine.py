378b545e525ce62a82360254835143e182997ae7
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 15:28:32 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -14,6 +14,9 @@ def Movement():
  while (FruitPresence == 'fruit'):
   move()
   FruitPresence = touch()
+  NewSource = look()
+ if NewSource == 'fruit':
+  Movement()  
  if FruitPresence != 'fruit':
   turn(-1)
   Movement()
