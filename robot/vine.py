806f30a8e41c2c4ad0debba3b8ba7acd95813f6f
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 15:17:17 +0100

    Running robot/vine.py

--- robot/vine.py
+++ robot/vine.py
@@ -7,3 +7,9 @@ from tealight.robot import (move,
                             right_side)
 
 # Add your code here
+def Movement
+
+ FruitPresence = touch()
+  
+  If FruitPresence == 'fruit'
+   move()
