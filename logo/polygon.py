7dac23d3e50cffd2b59bb4a93e381ccd669dd931
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 11:50:17 +0100

    Running logo/polygon.py

--- logo/polygon.py
+++ logo/polygon.py
@@ -1,9 +1,9 @@
 from tealight.logo import move, turn
 
-def polygon(edges, size):
-  angle = 360.0 / edges
-  for i in range(0, edges):
-    move(size)
-    turn(angle)
+def polygon(edges, size):  #function with 2 vars, edges and size
+  angle = 360.0 / edges    #angle of rotation = degrees in circle/number of edges
+  for i in range(0, edges):#while i is less than number edges
+    move(size)             #move size
+    turn(angle)            #turn value of angle
     
-polygon(360,1)
+polygon(360,1)             #call function
