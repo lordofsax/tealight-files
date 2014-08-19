9603238b1e34f1eb8d891a07e87cdb67a3b77934
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 11:54:26 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -4,11 +4,11 @@ from tealight.logo import move, turn
 def square(side):
   for i in range(0,4):
     move(side)
-    turn(90)
+    turn(60)
 
 def waterwheel(edges, size):  #side = sidelength
   angle = 360 / edges      #finds angle between each edge
-  decoration = size / 2
+  decoration = size / 2  #makes square side length half size
   for i in range(0, edges):
     move(size)
     square(decoration)
