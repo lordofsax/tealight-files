20fe274cd4a7b8a1bdb443527a5ebd69a7795d5b
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 11:55:55 +0100

    Running logo/waterwheel.py

--- logo/waterwheel.py
+++ logo/waterwheel.py
@@ -2,7 +2,7 @@ from tealight.logo import move, turn
 
 
 def square(side):
-  for i in range(0,3):
+  for i in range(0,3):  #repeats this many times, should equal number of edges of decoration
     move(side)
     turn(120)
 
