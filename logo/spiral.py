487eb83b1c35cff32cef8fa69d240f679e5ee39a
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 11:44:28 +0100

    Running logo/spiral.py

--- logo/spiral.py
+++ logo/spiral.py
@@ -6,7 +6,7 @@ def spiral(size):  #function with var size
     return
   
   move(size)       #move  size pixels
-  turn(0.1)         #rotate
+  turn(90)         #rotate
   spiral(size + 5) #increase size by 5
   
 spiral(0)          #call spiral with size = 0
