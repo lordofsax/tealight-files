504ae80d924a59d52982558e18a922c3883769dc
lordofsax <lordofsax@users.noreply.github.com>
2014-08-19 12:13:10 +0100

    Running logo/chessboard.py

--- logo/chessboard.py
+++ logo/chessboard.py
@@ -4,21 +4,7 @@ def chessboard(sizeT):
  for i in range(0,4):
   move(sizeT)
   turn(90)
-  
- size = sizeT * 7/8 
- for i in range(0,3): 
-  move(size)
-  turn(90)
-  
- size = sizeT * 6/8 
- for i in range(0,3): 
-  move(size)
-  turn(90) 
-  
- size = sizeT * 5/8 
- for i in range(0,3): 
-  move(size)
-  turn(90) 
+ 
    
   
 turn(-90)  
