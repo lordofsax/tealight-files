f1a0253003c5526533a88f9caa8cb51fc35657c3
lordofsax <lordofsax@users.noreply.github.com>
2014-08-20 12:29:06 +0100

    Running art/Memory_Game.py

--- art/Memory_Game.py
+++ art/Memory_Game.py
@@ -4,4 +4,5 @@ from tealight.art import (screen_width, screen_height)
 
 image(100,200, "animals/Bear.png")
 image(200,200, "animals/Bear.png")
-
+image(100,100, "animals/Cat.png")
+image(200,100, "animals/Cat.png")
