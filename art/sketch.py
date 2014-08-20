19ad124fce9deb5285de99f8da37db7ebae93b1f
lordofsax <lordofsax@users.noreply.github.com>
2014-08-20 10:25:47 +0100

    Running art/sketch.py

--- art/sketch.py
+++ art/sketch.py
@@ -18,4 +18,9 @@ def handle_mousemove(x,y,button):
     line(lastx, lasty, x, y)
     lastx = x
     lasty = y
+    
+  if button == "right":
+    line(lastx, lasty, x, y)
+    lastx = x
+    lasty = y
   
