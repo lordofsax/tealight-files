7236486fe0c95ae20d3f52415eeacecde066085b
lordofsax <lordofsax@users.noreply.github.com>
2014-08-20 12:29:46 +0100

    Running art/Memory_Game.py

--- art/Memory_Game.py
+++ art/Memory_Game.py
@@ -2,7 +2,11 @@ from tealight.art import (color, line, spot, circle, box, image, text, backgroun
 
 from tealight.art import (screen_width, screen_height)
 
-image(100,200, "animals/Bear.png")
-image(200,200, "animals/Bear.png")
-image(100,100, "animals/Cat.png")
-image(200,100, "animals/Cat.png")
+image(100,100, "animals/Bear.png")
+image(200,100, "animals/Bear.png")
+
+image(100,200, "animals/Cat.png")
+image(200,200, "animals/Cat.png")
+
+image(100,300, "animals/Dog.png")
+image(200,300, "animals/Dog.png")
