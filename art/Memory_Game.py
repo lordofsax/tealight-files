ed8ff24fb48b6323da6271ea86b0491ff74ab704
lordofsax <lordofsax@users.noreply.github.com>
2014-08-20 12:27:47 +0100

    Running art/Memory_Game.py

--- art/Memory_Game.py
+++ art/Memory_Game.py
@@ -1,4 +1,4 @@
 from tealight.art import (color, line, spot, circle, box, image, text, background)
 
 from tealight.art import (screen_width, screen_height)
-image(200,200, "animals/bear.png")
+image(200,200, "animals/Bear.png")
