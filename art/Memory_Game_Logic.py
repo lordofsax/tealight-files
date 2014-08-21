6a1961099f7ed03c86064608b322cc649c2ca798
lordofsax <lordofsax@users.noreply.github.com>
2014-08-21 14:20:39 +0100

    Running art/Memory_Game_Logic.py

--- art/Memory_Game_Logic.py
+++ art/Memory_Game_Logic.py
@@ -20,7 +20,7 @@ if cards_clicked < 2:
     
     first_card = name2  #possibly need new variable or to reference deck[]
     
-elif cards_clicked = 2:
+  elif cards_clicked = 2:
   
     if first_card = name2:  #possibly need new variable or to reference deck[]
     
