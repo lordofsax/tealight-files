b733e4f86e0120659c4ec42bad3135f1317f1ed7
lordofsax <lordofsax@users.noreply.github.com>
2014-08-21 10:30:04 +0100

    Running art/Memory_Game.py

--- art/Memory_Game.py
+++ art/Memory_Game.py
@@ -11,8 +11,10 @@ image(200,200, "animals/Cat.png")
 image(100,300, "animals/Dog.png")
 image(200,300, "animals/Dog.png")
 
-image(100,400, "animals/Dog.png")
-image(200,400, "animals/Dog.png")
+image(100,400, "animals/Elephant.png")
+image(200,400, "animals/Elephant.png")
 
-image(100,500, "animals/Dog.png")
-image(200,500, "animals/Dog.png")
+image(100,500, "animals/Frog.png")
+image(200,500, "animals/Frog.png")
+
+image
