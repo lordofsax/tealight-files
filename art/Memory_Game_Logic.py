37e1d1d7ffaea2e6c7c350ca62cc88e31a6f2723
lordofsax <lordofsax@users.noreply.github.com>
2014-08-21 14:18:50 +0100

    Running art/Memory_Game_Logic.py

--- art/Memory_Game_Logic.py
+++ art/Memory_Game_Logic.py
@@ -1,1 +1,35 @@
-print "A new file!"
+from tealight.art import (color, line, spot, circle, box, image, text, background)
+
+from tealight.art import (screen_width, screen_height)
+
+from random import shuffle
+
+cards_clicked =0
+
+score = 0
+
+#when a card has been clicked
+
+  global cards_clicked
+  
+  first_card = None
+
+  if cards_clicked < 2
+  
+    cards_clicked = cards_clicked + 1
+    
+    first_card = name2  #possibly need new variable or to reference deck[]
+    
+  elif cards_clicked = 2
+  
+    if first_card = name2  #possibly need new variable or to reference deck[]
+    
+      score = score + 1
+      
+      cards_clicked = 0
+      
+    else
+    
+      #flip cards back over
+    
+    
