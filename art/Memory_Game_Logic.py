db64300d9a27ac50a05e4b4ab7fa42f65c539e5a
lordofsax <lordofsax@users.noreply.github.com>
2014-08-21 14:20:22 +0100

    Running art/Memory_Game_Logic.py

--- art/Memory_Game_Logic.py
+++ art/Memory_Game_Logic.py
@@ -10,17 +10,17 @@ score = 0
 
 #when a card has been clicked
 
-  #global cards_clicked
+global cards_clicked
   
-  first_card = "none"
+first_card = None
 
-  if cards_clicked < 2:
+if cards_clicked < 2:
   
     cards_clicked = cards_clicked + 1
     
     first_card = name2  #possibly need new variable or to reference deck[]
     
-  elif cards_clicked = 2:
+elif cards_clicked = 2:
   
     if first_card = name2:  #possibly need new variable or to reference deck[]
     
